2fcd681bccf2f5f5016c24fd939dd87f16347338
wxclsk <115397280+wxclsk@users.noreply.github.com>
2022-10-16 12:32:20 +0300

    game.py

---
+++ https:/game.py/game.py
@@ -0,0 +1,17 @@
+# підключення бібліотек
+from pygame import*
+
+# клас-батько
+class GameSprite(sprite.Sprite):
+    def __init__(self):
+        pass
+    def reset(self):
+        pass
+# клас для ракеток
+class Player(GameSprite):
+    def update_right(self):
+        pass
+    def update_left(self):
+        pass
+        
+win_width
