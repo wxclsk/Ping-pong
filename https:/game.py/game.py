99825d1a1cb7a7e78d676179d9bff579a8186a2e
wxclsk <115397280+wxclsk@users.noreply.github.com>
2022-10-16 12:38:00 +0300

    Update game.py

--- https:/game.py/game.py
+++ https:/game.py/game.py
@@ -14,4 +14,9 @@ class Player(GameSprite):
     def update_left(self):
         pass
         
-win_width
+win_width = 600
+win_height = 500
+
+window = display.set_mode(win_width,win_height)
+fon = (200,255,255)
+window.fill(fon)
