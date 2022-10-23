3d6854d23fc9cdbd915582a3f681de33aa57f9bc
wxclsk <115397280+wxclsk@users.noreply.github.com>
2022-10-23 13:03:51 +0300

    Update game.py

--- https:/game.py/game.py
+++ https:/game.py/game.py
@@ -3,17 +3,28 @@ from pygame import*
 
 # клас-батько
 class GameSprite(sprite.Sprite):
-    def __init__(self):
-        pass
+    def __init__(self,player_image,p_x,p_y,p_speed,width,height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(widht,height))
+        self.speed = p_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = p_x
+        self.rect.y = p_y
     def reset(self):
-        pass
+        window.blit(self.image,(self.rect.x,self.rect.y))
 # клас для ракеток
 class Player(GameSprite):
     def update_right(self):
-        pass
+        keys  = key.get_pressed()
+        if [K_UP] and self.rect.y >5:
+            self.rect.y -= self.speed
+        if [K_DOWN] and self.rect.y < 420: 
+            self.rect.y += self.speed           
     def update_left(self):
         pass
-        
+racket_right = Player("racket_r.png",520,200,4,50,150)
+racket_left = Player("racket_l.png",400,200,4,50,150)
+ball = GameSprite("ball.png",200,200,4,50,50)
 win_width = 600
 win_height = 500
 
@@ -33,6 +44,11 @@ while game:
             game = False
     if finish != True
         window.fill(fon)
+        racket_right.update_right()
+        racket_left.update_left()
+        rocket_right.reset()
+        rocket_left.reset()
+        ball.reset()   
     display.update()
     clock.tick(FPS)
  
